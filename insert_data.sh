7927cc8b5afe78d7d4560093591611f75d60b28d
Artemii Kravtsov <artyomkravtsov@gmail.com>
2022-05-20 14:42:25 +0300

    Create insert_data.sh

---
+++ insert_data.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+SUCCESSFUL_INSERT_RESULT="INSERT 0 1"
+
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $WINNER != "winner" ]]
+  then
+    WINNER_ID=$($PSQL "SELECT team_id from teams where name='$WINNER'")
+
+    if [[ -z $WINNER_ID ]]
+    then
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+
+      if [[ $INSERT_OPPONENT_RESULT == $SUCCESSFUL_INSERT_RESULT ]]
+      then
+        echo "Inserted new team: $WINNER"
+      fi
+    fi
+
+    WINNER_ID=$($PSQL "SELECT team_id from teams where name='$WINNER'")
+  fi
+  
+  if [[ $OPPONENT != "opponent" ]]
+  then
+    OPPONENT_ID=$($PSQL "SELECT team_id from teams where name='$OPPONENT'")
+
+    if [[ -z $OPPONENT_ID ]]
+    then
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+
+      if [[ $INSERT_OPPONENT_RESULT == $SUCCESSFUL_INSERT_RESULT ]]
+      then
+        echo "Inserted new team: $OPPONENT"
+      fi
+    fi
+
+    OPPONENT_ID=$($PSQL "SELECT team_id from teams where name='$OPPONENT'")
+  fi
+
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+
+  if [[ $INSERT_GAME_RESULT == $SUCCESSFUL_INSERT_RESULT ]]
+  then
+    echo "Inserted game: $YEAR - $ROUND - $WINNER_ID - $OPPONENT_ID - $WINNER_GOALS - $OPPONENT_GOALS"
+  else
+    echo "Error inserting game: $YEAR - $ROUND - $WINNER_ID - $OPPONENT_ID - $WINNER_GOALS - $OPPONENT_GOALS"
+  fi
+done
